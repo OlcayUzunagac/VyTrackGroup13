9e7c58bc8b48fb8c8efc3500486557a7d1c71f8c
Aysun <kirmizigul052@gmail.com>
2020-10-20 14:45:11 +0200

    1st and 2nd test cases of 11th Userstory

--- src/test/java/com/Group13/tests/US_11/TC_1.java
+++ src/test/java/com/Group13/tests/US_11/TC_1.java
@@ -6,7 +6,26 @@ import org.testng.annotations.Test;
 
 public class TC_1 {
     //(US-11)Access vehicle service log by using Truck Driver's Credentials
-    //2.Verify that non authorised user should Not be able to access Vehicle service log
+
+    //TC --1. Verify that authorised user should be able to access and see all Vehicle service log
+    //(Note: authorised: Truck driver)
+    @Test
+    public void VytrackTC107() throws InterruptedException {
+        LoginPage loginPage = new LoginPage();
+
+        loginPage.loginAsDriver();
+
+        DashboardPage dashboardPage = new DashboardPage();
+
+        dashboardPage.navigateToModule("Fleet", "Vehicle Services Logs");
+
+        Thread.sleep(15000);
+    }
+
+
+
+
+        //2.Verify that non authorised user should Not be able to access Vehicle service log
     // (non authorise:Store manager, sales manager )
     @Test
     public void VytrackTC109() throws InterruptedException {
