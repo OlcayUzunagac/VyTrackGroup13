8edd8aba89fcc1d50e68da3c592a54c44dd4462b
ocavli <nukro20022002@gmail.com>
2020-10-22 16:23:37 +0200

    US7_TC1_TC2 created

---
+++ src/test/java/com/Group13/tests/US_7/TC1.java
@@ -0,0 +1,21 @@
+package com.Group13.tests.US_7;
+
+import com.Group13.pages.DashboardPage;
+import com.Group13.pages.LoginPage;
+import com.Group13.tests.TestBase;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TC1 extends TestBase {
+
+
+
+    @Test
+    public void TC1() throws InterruptedException {
+        System.out.println();
+    }
+
+
+
+
+}
