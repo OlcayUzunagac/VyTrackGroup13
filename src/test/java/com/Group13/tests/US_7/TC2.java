b7bed932d91cf3cc375224095fb7ca4cc30adeed
ocavli <nukro20022002@gmail.com>
2020-10-23 11:26:50 +0200

    User Stors 7 - Tc1 and Tc2 completed

--- src/test/java/com/Group13/tests/US_7/TC2.java
+++ src/test/java/com/Group13/tests/US_7/TC2.java
@@ -3,6 +3,7 @@ package com.Group13.tests.US_7;
 import com.Group13.pages.DashboardPage;
 import com.Group13.pages.LoginPage;
 import com.Group13.tests.TestBase;
+import com.Group13.utilities.BrowserUtils;
 import com.Group13.utilities.ConfigurationReader;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -13,8 +14,20 @@ public class TC2 extends TestBase {
 
     @Test
     public void TC2(){
-        System.out.println();
-        //
+        //2.Verify that Non authorized user should not be able to access Vehicle Contract
+         //       (Note: Truck driver)
+
+        //Open browser,Login as salesmanager
+        LoginPage loginPage = new LoginPage();
+        loginPage.loginAsDriver();
+        DashboardPage dashboardPage = new DashboardPage();
+        dashboardPage.navigateToModule("Fleet","Vehicle Contracts");
+        BrowserUtils.waitForPageToLoad(3);
+        Assert.assertNotEquals(dashboardPage.getPageSubTitle(),"All Vehicle Contract");
+        System.out.println("dashboardPage.getPageSubTitle() = " + dashboardPage.getPageSubTitle());
+
+
+
     }
 
 }
